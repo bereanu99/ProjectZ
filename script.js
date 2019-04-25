54215ba42b9456159520e8727eeda9c7c4822e9e
Marius <mariusb@ortec.om>
2019-04-25 11:11:21 +0300

    aa

--- script.js
+++ script.js
@@ -2,11 +2,38 @@ var app = angular.module('main',['ngRoute']);
 
 app.config(function($routeProvider){
     $routeProvider.when('/',{
-        templateUrl:'./components/home.html',
+        templateUrl:'./pages/home.html',
         controller:'homeCtrl',
+
+    }).when('/galerie',{
+        template:'./pages/galerie.html',
+        controller:'galerieCtrl',
+
+    }).when('/evenimente.html',{
+        template:'./pages/evenimente.html',
+        controller:'evenimenteCtrl',
+
+    }).when('/contact.html',{
+        template:'./pages/contact.html',
+        controller:'contactCtrl',
+    }).otherwise({
+        template: '404',
     })
 })
 
+
 app.controller('homeCtrl',function($scope,$location){
+    $scope.$location
+})
+
+app.controller('galerieCtrl',function($scope,$location){
+    $scope.$location
+})
+
+app.controller('evenimenteCtrl',function($scope,$location){
+    $scope.$location
+})
+
+app.controller('contactCtrl',function($scope,$location){
     $scope.$location
 })
